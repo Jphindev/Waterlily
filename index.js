5d89da3de482caf54369c7fefc1a3ef50090e33f
Jphindev <jphindev@gmail.com>
2024-07-03 16:15:28 +0200

    refactore js + add animals keyframes

--- index.js
+++ index.js
@@ -19,100 +19,81 @@ const finished = document.querySelector(".finished");
 const gamefield = document.querySelector(".gamefield");
 const safeplace = document.querySelector(".safeplace");
 
-// GAME WITH SNAIL
+// ANIMAL FUNCTION
 
-if (safe_snail.style.display === "block") {
-	snail.style.display = "none";
-}
+let startgame = function (animal, safe_animal, animation_property) {
+	// a leave off the start redisplay the animal
+	start.addEventListener("mouseleave", function () {
+		if (safe_animal.style.display === "block") {
+			animal.style.display = "none";
+		} else {
+			animal.style.display = "initial";
+		}
+
+		// a hover on waterlily trigger the animation
+		waterlily.addEventListener("mouseenter", function () {
+			if (gamefield.style.cursor !== "default") {
+				animal.style.display = "none";
+				waterlily.style.transform = "translateX(650px)";
+				waterlily.style.animation = animation_property;
+			}
+
+			// a leave off waterlily stop the animation
+			waterlily.addEventListener("mouseleave", function () {
+				waterlily.style.transform = "translateX(0)";
+				waterlily.style.animation = "waterlily_fail 0.5s linear";
+				if (safe_animal.style.display === "block") {
+					animal.style.display = "none";
+				} else {
+					animal.style.display = "initial";
+				}
+
+				// a hover on the finished saves the animal
+				finished.addEventListener("mouseenter", function () {
+					if (gamefield.style.cursor !== "default") {
+						animal.style.display = "none";
+						safe_animal.style.display = "block";
+						gamefield.style.cursor = "default";
+					}
+				});
+			});
+		});
+	});
+};
 
 snail.addEventListener("click", function () {
 	// the cursor takes the shape of the animal
 	gamefield.style.cursor = `url('icons/snail.svg') 25 25, auto`;
 	snail.style.display = "none";
-
-	// a leave off the start redisplay the animal
-	start.addEventListener("mouseleave", function () {
-		if (safe_snail.style.display === "block") {
-			snail.style.display = "none";
-		} else {
-			snail.style.display = "initial";
-		}
-
-		// a hover on waterlily trigger the animation
-		waterlily.addEventListener("mouseenter", function () {
-			if (gamefield.style.cursor !== "default") {
-				snail.style.display = "none";
-				waterlily.style.transform = "translateX(650px)";
-				waterlily.style.animation = "niv_snail 5s linear";
-			}
-
-			// a leave off waterlily stop the animation
-			waterlily.addEventListener("mouseleave", function () {
-				waterlily.style.transform = "translateX(0)";
-				waterlily.style.animation = "waterlily_fail 0.5s linear";
-				if (safe_snail.style.display === "block") {
-					snail.style.display = "none";
-				} else {
-					snail.style.display = "initial";
-				}
-
-				// a hover on the finished saves the animal
-				finished.addEventListener("mouseenter", function () {
-					if (gamefield.style.cursor !== "default") {
-						snail.style.display = "none";
-						safe_snail.style.display = "block";
-						gamefield.style.cursor = "default";
-					}
-				});
-			});
-		});
-	});
+	startgame(snail, safe_snail, "niv_snail 5s linear");
 });
-
-// GAME WITH TURTLE
 
 turtle.addEventListener("click", function () {
 	// the cursor takes the shape of the animal
 	gamefield.style.cursor = `url('icons/turtle.svg') 25 25, auto`;
 	turtle.style.display = "none";
+	startgame(turtle, safe_turtle, "niv_turtle 10s linear");
+});
 
-	// a leave off the start redisplay the animal
-	start.addEventListener("mouseleave", function () {
-		if (safe_turtle.style.display === "block") {
-			turtle.style.display = "none";
-		} else {
-			turtle.style.display = "initial";
-		}
+frog.addEventListener("click", function () {
+	// the cursor takes the shape of the animal
+	gamefield.style.cursor = `url('icons/frog.svg') 25 25, auto`;
+	frog.style.display = "none";
+	startgame(frog, safe_frog, "niv_frog 10s linear");
+});
 
-		// a hover on waterlily trigger the animation
-		waterlily.addEventListener("mouseenter", function () {
-			if (gamefield.style.cursor !== "default") {
-				turtle.style.display = "none";
-				waterlily.style.transform = "translateX(650px)";
-				waterlily.style.animation = "niv_turtle 10s linear";
-			}
+cat.addEventListener("click", function () {
+	// the cursor takes the shape of the animal
+	gamefield.style.cursor = `url('icons/cat.svg') 25 25, auto`;
+	cat.style.display = "none";
+	startgame(cat, safe_cat, "niv_cat 10s linear");
+});
 
-			// a leave off waterlily stop the animation
-			waterlily.addEventListener("mouseleave", function () {
-				waterlily.style.transform = "translateX(0)";
-				waterlily.style.animation = "waterlily_fail 0.5s linear";
-				if (safe_turtle.style.display === "block") {
-					turtle.style.display = "none";
-				} else {
-					turtle.style.display = "initial";
-				}
-
-				// a hover on the finished saves the animal
-				finished.addEventListener("mouseenter", function () {
-					if (gamefield.style.cursor !== "default") {
-						turtle.style.display = "none";
-						safe_turtle.style.display = "block";
-						gamefield.style.cursor = "default";
-					}
-				});
-			});
-		});
-	});
+dog.addEventListener("click", function () {
+	// the cursor takes the shape of the animal
+	gamefield.style.cursor = `url('icons/dog.svg') 25 25, auto`;
+	dog.style.display = "none";
+	startgame(dog, safe_dog, "niv_dog 10s linear");
 });
 
 // The cursor return to default when it is over the water
